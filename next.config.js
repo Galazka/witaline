6c34316203394390b1c40452c933863b52806048
Galazka <tomekgalazka@gmail.com>
2026-06-17 23:18:35 +0200

    Add @supabase/ssr, fix next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-﻿const nextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  i18n: { locales: ["pl", "en"], defaultLocale: "pl" },
   env: { NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL }
 };
 module.exports = nextConfig;
