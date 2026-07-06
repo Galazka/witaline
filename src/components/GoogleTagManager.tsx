f6cb73e6501862d5b803eabe021e46cccb232996
Galazka <tomekgalazka@gmail.com>
2026-07-06 11:32:46 +0200

    fix: GTM hardcoded ID, beforeInteractive strategy for head injection

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -2,22 +2,21 @@
 
 import Script from "next/script";
 
-export default function GoogleTagManager() {
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
-  if (!gtmId) return null;
+const GTM_ID = "GTM-WJK7NKFZ";
 
+export default function GoogleTagManager() {
   return (
     <>
       <Script
         id="gtm-script"
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`,
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
         }}
       />
       <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
